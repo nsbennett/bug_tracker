339bcc95f83e6a37becb775bccc20ebbe35e78ba
Nathan Bennett <nathan@nathansbennett.com>
2023-04-03 23:34:13 -0500

    rolling back slack integration

--- base_app/views.py
+++ base_app/views.py
@@ -20,18 +20,18 @@ from slack_sdk.errors import SlackApiError
 
 # Create your views here.
 
-def post_to_slack(user, message):
-    slack_token = os.environ["SLACK_BOT_TOKEN"]
-    client = WebClient(token=slack_token)
+# def post_to_slack(user, message):
+#     slack_token = os.environ["SLACK_BOT_TOKEN"]
+#     client = WebClient(token=slack_token)
 
-    try:
-        response = client.chat_postMessage(
-            channel=os.environ["SLACK_CHANNEL_ID"], 
-            text=f"Post by {user}: {message}")
+#     try:
+#         response = client.chat_postMessage(
+#             channel=os.environ["SLACK_CHANNEL_ID"], 
+#             text=f"Post by {user}: {message}")
     
-    except SlackApiError as e:
-    # You will get a SlackApiError if "ok" is False
-        assert e.response["error"]
+#     except SlackApiError as e:
+#     # You will get a SlackApiError if "ok" is False
+#         assert e.response["error"]
 
 
 def view_home(request):
@@ -107,8 +107,8 @@ def submit_ticket(request):
             submission = ticket_form.save(commit=False)
             submission.ticket_author = request.user
             submission.save()
-            if request.user.username == "development":
-                post_to_slack("development", submission)
+            # if request.user.username == "development":
+            #     post_to_slack("development", submission)
             messages.success(request, ("Done!"))
             return redirect('user_tickets')
         else:
